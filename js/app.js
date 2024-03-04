0ba17ec392b32ed46f99595577d61901bfbac0ab
tarrgabor <80397144+tarrgabor@users.noreply.github.com>
2024-03-04 08:42:52 +0100

    Színváltó folytatása
    90%ban kész, a navbar valamiért kék

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
-//mode switch
+/*function toggleMode() {
+  var dropdown = document.getElementById("modeDropdown");
+  var selectedMode = dropdown.value;
+
+  document.body.classList.toggle("dark-mode", selectedMode === "dark");
+  document.getElementsByClassName("navbar").classList.toggle("dark-mode", selectedMode === "dark");
+  document.getElementsByClassName("footer").classList.toggle("dark-mode", selectedMode === "dark");
+}*/
 
 function toggleMode() {
-    var dropdown = document.getElementById("modeDropdown");
-    var selectedMode = dropdown.value;
+  var dropdown = document.getElementById("modeDropdown");
+  var selectedMode = dropdown.value;
 
-    // Toggle dark mode class on body
-    document.body.classList.toggle("dark-mode", selectedMode === "dark");
-    document.nav.classList.toggle("dark-mode", selectedMode === "dark");
+  document.body.classList.toggle("dark-mode", selectedMode === "dark");
+  
+  var navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("dark-mode", selectedMode === "dark");
+  
+  var footers = document.getElementsByClassName("footer");
+  for (var j = 0; j < footers.length; j++) {
+    footers[j].classList.toggle("dark-mode", selectedMode === "dark");
   }
+}
