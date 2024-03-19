9932a5c90945ea55bba2ebe5f8f57c95f2c9f7e5
mojzeservin <113088620+mojzeservin@users.noreply.github.com>
2024-03-19 08:46:56 +0100

    adagszabályzó gomb implementálása

--- js/alkoholo.js
+++ js/alkoholo.js
@@ -1,3 +1,4 @@
+//Sex on the Beach
 let BeachPlus = document.querySelector("#BeachPlus");
 let BeachMinus = document.querySelector("#BeachMinus");
 let BeachVodka = document.querySelector("#BeachVodka");
@@ -30,5 +31,93 @@ BeachMinus.addEventListener('click', function(){
     BeachAfonya.innerHTML = Number(BeachAfonya.innerHTML) - 4;
 
     BeachCount.innerText = Number(BeachCount.innerHTML) - 1;
+    }
+});
+
+//Pina colada
+let ColadaPlus = document.querySelector("#ColadaPlus");
+let ColadaMinus = document.querySelector("#ColadaMinus");
+let Crum = document.querySelector("#Crum");
+let CKlikor = document.querySelector("#CKlikor");
+let CananaszLe = document.querySelector("#CananaszLe");
+let Ctejszin = document.querySelector("#Ctejszin");
+let Cjeg = document.querySelector("#Cjeg");
+let CananaszKo = document.querySelector("#CananaszKo");
+
+ColadaPlus.addEventListener('click', function(){
+    ColadaCount.innerText = Number(ColadaCount.innerHTML) + 1;
+
+    Crum.innerText = Number(Crum.innerHTML) + 4;
+    CKlikor.innerHTML = Number(CKlikor.innerHTML) + 2 ;
+    CananaszLe.innerHTML = Number(CananaszLe.innerHTML) + 1;
+    Ctejszin.innerHTML = Number(Ctejszin.innerHTML) + 2;
+    Cjeg.innerHTML = Number(Cjeg.innerHTML) + 4;
+    CananaszKo.innerHTML = Number(CananaszKo.innerHTML) + 1;
+});
+
+ColadaMinus.addEventListener('click', function(){
+    if (ColadaCount.innerHTML==1) {
+        ColadaCount.innerText = 1;
+
+        Crum.innerText = Number(Crum.innerHTML) / 1;
+        CKlikor.innerHTML = Number(CKlikor.innerHTML) / 1;
+        CananaszLe.innerHTML = Number(CananaszLe.innerHTML) / 1;
+        Ctejszin.innerHTML = Number(Ctejszin.innerHTML) / 1;
+        Cjeg.innerHTML = Number(Cjeg.innerHTML) / 1;
+        CananaszKo.innerHTML = Number(CananaszKo.innerHTML) / 1;
+    }
+    else{
+        Crum.innerText = Number(Crum.innerHTML) - 4;
+        CKlikor.innerHTML = Number(CKlikor.innerHTML) - 2 ;
+        CananaszLe.innerHTML = Number(CananaszLe.innerHTML) - 1;
+        Ctejszin.innerHTML = Number(Ctejszin.innerHTML) - 2;
+        Cjeg.innerHTML = Number(Cjeg.innerHTML) - 4;
+        CananaszKo.innerHTML = Number(CananaszKo.innerHTML) - 1;
+
+    ColadaCount.innerText = Number(ColadaCount.innerHTML) - 1;
+    }
+});
+
+//Mojito
+/*let ColadaPlus = document.querySelector("#ColadaPlus");
+let ColadaMinus = document.querySelector("#ColadaMinus");
+let Crum = document.querySelector("#Crum");
+let CKlikor = document.querySelector("#CKlikor");
+let CananaszLe = document.querySelector("#CananaszLe");
+let Ctejszin = document.querySelector("#Ctejszin");
+let Cjeg = document.querySelector("#Cjeg");
+let CananaszKo = document.querySelector("#CananaszKo");*/
+
+MojitoPlus.addEventListener('click', function(){
+    MojitoCount.innerText = Number(MojitoCount.innerHTML) + 1;
+
+    Crum.innerText = Number(Crum.innerHTML) + 4;
+    CKlikor.innerHTML = Number(CKlikor.innerHTML) + 2 ;
+    CananaszLe.innerHTML = Number(CananaszLe.innerHTML) + 1;
+    Ctejszin.innerHTML = Number(Ctejszin.innerHTML) + 2;
+    Cjeg.innerHTML = Number(Cjeg.innerHTML) + 4;
+    CananaszKo.innerHTML = Number(CananaszKo.innerHTML) + 1;
+});
+
+MojitoMinus.addEventListener('click', function(){
+    if (MojitoCount.innerHTML==1) {
+        MojitoCount.innerText = 1;
+
+        Crum.innerText = Number(Crum.innerHTML) / 1;
+        CKlikor.innerHTML = Number(CKlikor.innerHTML) / 1;
+        CananaszLe.innerHTML = Number(CananaszLe.innerHTML) / 1;
+        Ctejszin.innerHTML = Number(Ctejszin.innerHTML) / 1;
+        Cjeg.innerHTML = Number(Cjeg.innerHTML) / 1;
+        CananaszKo.innerHTML = Number(CananaszKo.innerHTML) / 1;
+    }
+    else{
+        Crum.innerText = Number(Crum.innerHTML) - 4;
+        CKlikor.innerHTML = Number(CKlikor.innerHTML) - 2 ;
+        CananaszLe.innerHTML = Number(CananaszLe.innerHTML) - 1;
+        Ctejszin.innerHTML = Number(Ctejszin.innerHTML) - 2;
+        Cjeg.innerHTML = Number(Cjeg.innerHTML) - 4;
+        CananaszKo.innerHTML = Number(CananaszKo.innerHTML) - 1;
+
+    MojitoCount.innerText = Number(MojitoCount.innerHTML) - 1;
     }
 });
